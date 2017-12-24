8d4ae4ddfec48f43ba5a7eea2947f4bd83792083
yejingjing <18879908476@163.com>
2017-12-24 19:26:37 +0800

    gggg

---
+++ src/Java/Demo2.java
@@ -0,0 +1,13 @@
+package Java;
+
+/**
+ * Author:YeJingJing
+ * Create Date:2017-12-24 17:56
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println("i="+i);
+        }
+    }
+}
